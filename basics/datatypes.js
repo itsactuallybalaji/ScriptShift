f21f231fa26cfa13b536aef719e9e4c27d51f4a0
itsactuallybalaji <itsactuallybalaji@gmail.com>
2026-02-24 00:45:43 +0530

    My First JS Git Commit

---
+++ basics/datatypes.js
@@ -0,0 +1,14 @@
+// JS is a dynamic language. let is a keyword to define anything and also we can change the value of it
+// Initialising a number
+let number = 5
+// Initialising a String
+let names = 'Java Script'
+// Initialising a boolean
+let bools = true
+// Initialising a character
+let initial = 'C'
+// Printing all of them. use node filename.js to run the code in the terminal
+console.log('number: '+number);
+console.log('name: '+names);
+console.log('boolean: '+bools);
+console.log('char: '+initial);
