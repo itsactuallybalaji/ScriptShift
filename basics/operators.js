d578715780e10ddbff7473d4fc76bc8d1c83968d
itsactuallybalaji <itsactuallybalaji@gmail.com>
2026-02-24 01:00:05 +0530

    Operator learning

---
+++ basics/operators.js
@@ -0,0 +1,8 @@
+// Arithmetic operators
+let a = 5
+let b = 6
+console.log('SUM: '+(a+b))
+console.log('Difference: '+ (a-b))
+console.log('Product: '+(a*b))
+console.log('Division: '+(a/b))
+console.log('Modulous: '+(a%b))
